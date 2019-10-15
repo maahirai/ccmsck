a2ffda4349e21a43d13cd975b6a50b752a505384
musuchika <musu.tec@gmail.com>
2019-10-15 17:31:23 +0900

    エラーコメントの改良

--- ccmsck.c
+++ ccmsck.c
@@ -24,10 +24,16 @@ Token *token;
 
 //エラーが起きたことを知らせる関数
 //printfと同じ引数
-void error(char *fmt, ...){
+char *user_input;
+void error_at(char *loc, char *fmt, ...){
     va_list ap;
     va_start(ap, fmt);
-    vfprintf(stderr,fmt,ap);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, " ");
+    fprintf(stderr, "^ ");
+    vfprintf(stderr, fmt, ap);
     fprintf(stderr, "\n");
     exit(1);
 }
@@ -45,7 +51,7 @@ bool consume(char op){
 //そうでない場合はエラーを報告する．
 void expect(char op){
     if(token->kind != TK_RESERVED || token->str[0] != op)
-        error("%cではありません．",op);
+        error_at(token->str,"%cではありません．",op);
     token=token->next;
 }
 
@@ -53,7 +59,7 @@ void expect(char op){
 //そうでない場合はエラーを報告する．
 int expect_number(){
     if(token->kind != TK_NUM)
-        error("数値ではありません．");
+        error_at(token->str, "数値ではありません．");
     int value = token->value;
     token = token->next;
     return value;
@@ -96,14 +102,18 @@ Token *tokenize(char *p){
             continue;
         }
 
-        error("トークナイズできません.");
+        error_at(p,"トークナイズできません.");
     }
 
     new_token(TK_EOF,cur,p);
     return top.next;
 }
 
+
+
+
 int main(int argc, char **argv) {
+    user_input = argv[1];
     if (argc != 2) {
         fprintf(stderr, "引数の個数が正しくありません\n");
         return 1;
